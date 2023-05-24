ca43a3bd26014d68cbf580f231aa0e4428016b81
sitialiyahyah <sitialiyah94.net@gmail.com>
2023-05-24 11:37:37 +0800

    Membuat Class Aksi dan Method main

---
+++ src/praktek001/MahasiswaaAksi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek001;
+
+/**
+ *
+ * @author asus
+ */
+public class MahasiswaaAksi {
+    public static void main(String[] args) {
+        
+    }
+}
